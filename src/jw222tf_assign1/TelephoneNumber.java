c8ee1dfad409d143dab21589ff08071ceeadbbc6
Jorian <jorian@outlook.com>
2016-11-03 21:39:37 +0100

    New assignment

--- src/jw222tf_assign1/TelephoneNumber.java
+++ src/jw222tf_assign1/TelephoneNumber.java
@@ -3,11 +3,15 @@ package jw222tf_assign1;
 import java.util.Random;
 
 /**
- * Created by Jorian on 3-11-2016.
+ * Created by Jorian on 3-11-2016 at 21:37.
  */
 public class TelephoneNumber {
     public static void main(String[] args) {
-        Random digits
+        Random digits = new Random();
+        String telephoneNumber = "0704-" + String.format("%s", digits.nextInt(9) + 1);
+        for (int i = 0; i < 5; i++) {
+            telephoneNumber += String.format("%s", digits.nextInt(9));
+        }
         System.out.println("Random telephone number: " + telephoneNumber);
     }
 }
