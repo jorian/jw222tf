c8b185d7ccaec69a6310c40ac6c76987841d6d07
Jorian <jorian@outlook.com>
2016-11-03 22:08:42 +0100

    New assignment

---
+++ src/jw222tf_assign1/Distance.java
@@ -0,0 +1,29 @@
+package jw222tf_assign1;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ * Created by Jorian on 3-11-2016 at 21:40.
+ */
+public class Distance {
+    public static void main(String[] args) {
+        System.out.print("Enter the x-coordinate of the first coordinates: ");
+        Scanner x1Input = new Scanner(System.in);
+        int x1 = x1Input.nextInt();
+        System.out.print("Enter the y-coordinate of the first coordinates: ");
+        Scanner y1Input = new Scanner(System.in);
+        int y1 = y1Input.nextInt();
+        System.out.print("Enter the x-coordinate of the second coordinates: ");
+        Scanner x2Input = new Scanner(System.in);
+        int x2 = x2Input.nextInt();
+        System.out.print("Enter the y-coordinate of the second coordinates: ");
+        Scanner y2Input = new Scanner(System.in);
+        int y2 = y2Input.nextInt();
+
+        double calculation = ((x2 - x1)^2) + ((y2-y1)^2);
+        double distance = Math.sqrt(calculation); // (double(x2-x1)^2 + (y2-y1)^2);
+        System.out.printf("Value: %.3f", distance);
+    }
+}
+
