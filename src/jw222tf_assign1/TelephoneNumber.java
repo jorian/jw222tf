5f1e2bbf9a45c748d3d479b5cce93ff823e5e932
Jorian <jorian@outlook.com>
2016-11-03 21:06:02 +0100

    New assignment

---
+++ src/jw222tf_assign1/TelephoneNumber.java
@@ -0,0 +1,13 @@
+package jw222tf_assign1;
+
+import java.util.Random;
+
+/**
+ * Created by Jorian on 3-11-2016.
+ */
+public class TelephoneNumber {
+    public static void main(String[] args) {
+        Random digits
+        System.out.println("Random telephone number: " + telephoneNumber);
+    }
+}
