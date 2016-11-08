918391cf5fad81668446697ad6ae5d380436f324
Jorian <jorian@outlook.com>
2016-11-08 11:29:39 +0100

    changed scanner

--- src/jw222tf_assign1/Convert.java
+++ src/jw222tf_assign1/Convert.java
@@ -7,12 +7,15 @@ import java.util.Scanner;
 
 public class Convert {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Please enter the amount of Fahrenheit: ");
-        Scanner fahrenheitInput = new Scanner(System.in);
-        double fahrenheit = fahrenheitInput.nextDouble();
+        double fahrenheit = sc.nextDouble();
         double celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
         System.out.printf(fahrenheit + " °F converts to %.1f", celsius);
         System.out.print(" °C.");
+
+        sc.close();
         // It is not only Swedes who suffer from the different use of commas and dots...
     }
 }
