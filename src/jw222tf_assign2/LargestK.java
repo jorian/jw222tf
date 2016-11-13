cc1e8980b2b0f51b0c381422e9c00a04cf2d6f10
Jorian <jorian@outlook.com>
2016-11-13 15:55:17 +0100

    Initial commit

---
+++ src/jw222tf_assign2/LargestK.java
@@ -0,0 +1,27 @@
+package jw222tf_assign2;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+/**
+ * Created by Jorian on 13-11-2016 at 15:30.
+ */
+public class LargestK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+
+        int k=0,l=0, input = sc.nextInt();
+
+        while(true) {
+            if((k + (l + 2)) < input) {
+                l += 2;
+                k += l;
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println(l);
+    }
+}
