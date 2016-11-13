e1bf8b1e0bae14d796d978aa5b3ae4b9c49e74d8
Jorian <jorian@outlook.com>
2016-11-13 16:47:08 +0100

    Initial commit

--- src/jw222tf_assign2/HighLow.java
+++ src/jw222tf_assign2/HighLow.java
@@ -10,23 +10,44 @@ public class HighLow {
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter a number to check: ");
-        int input = sc.nextInt();
         int random = rand.nextInt(99);
         int tries = 1;
+        int input;
 
-        while (input != random) {
-            tries++;
-            if (input > random) {
-                System.out.println("lower");
-            }
-            else {
-                System.out.println("higher");
-            }
+        for (int i = 0;i<10;i++) {
             System.out.print("Please enter a number to check: ");
             input = sc.nextInt();
+            if (input == random) {
+                System.out.println("The correct number is indeed: " + random + ". Guessed correctly after " + (tries) + " tries!");
+                break;
+            } else if (input > random) {
+                System.out.println("lower");
+            } else {
+                System.out.println("higher");
+            }
+            //System.out.print("Please enter a number to check: ");
+            //input = sc.nextInt();
         }
-        System.out.println("The correct number is indeed: " + random + ". Guessed correctly after " + (tries) + " tries!");
+
+
+       /*
+        while ((input != random) && (tries < 10)) {
+            tries++;
+            if (tries < 10) {
+                if (input > random) {
+                    System.out.println("lower");
+                } else {
+                    System.out.println("higher");
+                }
+                System.out.print("Please enter a number to check: ");
+                input = sc.nextInt();
+            }
+            else {
+                System.out.println("Unfortunately, you failed to guess within 10 tries!");
+                break;
+            }
+        }
+        System.out.println("The correct number is indeed: " + random + ". Guessed correctly after " + (tries) + " tries!"); */
     }
 }
 
