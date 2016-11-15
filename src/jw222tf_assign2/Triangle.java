a082101b28de1eb9064d2991888e0ef3d27ec293
Jorian <jorian@outlook.com>
2016-11-15 14:22:14 +0100

    Updates

---
+++ src/jw222tf_assign2/Triangle.java
@@ -0,0 +1,48 @@
+package jw222tf_assign2;
+
+import java.util.Scanner;
+
+/**
+ * Created by Jorian on 15-11-2016 at 13:44.
+ */
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter an odd integer: ");
+        int input = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        int count = 0, position;
+
+        if ((input%2) != 0) {
+            for (int i = 0; i <= input; i++) {
+                sb.append(" ");
+            }
+            for (int i = (input); i > 0; i--) {
+                sb.replace(i-1,i,"*");
+                System.out.println(sb);
+            }
+        } else {
+            System.out.println("The number is not an odd number.");
+        }
+
+        if ((input%2) != 0) {
+            for (int i = 0; i < input; i++) {
+                if (i == input / 2) {
+                    sb2.append("*");
+                }
+                else {
+                    sb2.append(" ");
+                }
+            }
+            System.out.println(sb2);
+            for (int i = (input / 2) - 1; i >= 0; i--) {
+                position = (input / 2);
+                count++;
+                sb2.replace((position + count),position + count + 1,"*");
+                sb2.replace((position - count),position - count + 1,"*");
+                System.out.println(sb2);
+            }
+        }
+    }
+}
