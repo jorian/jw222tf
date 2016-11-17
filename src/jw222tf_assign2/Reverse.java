d7cf2320eab83f88993c1376377322c68e4abff3
jorian <jorian@outlook.com>
2016-11-17 14:04:42 +0100

    yo

--- src/jw222tf_assign2/Reverse.java
+++ src/jw222tf_assign2/Reverse.java
@@ -9,8 +9,22 @@ public class Reverse {
                 's', 'i', 'h', 'T' };
 
         for (int i=0;i<text.length;i++) {
-            System.out.println(i);
+            System.out.print(String.valueOf(text[i]));
         }
+
+        char temp;
+        int left, right;
+        right = text.length - 1;
+        for (left = 0;left<right;left++,right--) {
+            temp = text[left];
+            text[left] = text[right];
+            text[right] = temp;
+        }
+        System.out.println();
+
+        for (char c : text) {
+            System.out.print(c);        }
+
 
         // Continue here ...
     }
