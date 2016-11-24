b8763994c1a44e880fa24294bfe49a3e13b7cc3d
Jorian <jorian@outlook.com>
2016-11-24 21:42:15 +0100

    Commit

--- src/jw222tf_assign2/Reverse.java
+++ src/jw222tf_assign2/Reverse.java
@@ -5,6 +5,8 @@ package jw222tf_assign2;
  */
 public class Reverse {
     public static void main(String [] args) {
+        int left, right;
+        char temp;
         char[] text = { '!', 'y', 's', 'a', 'E', ' ', 's', 'a', 'w', ' ',
                 's', 'i', 'h', 'T' };
 
@@ -12,15 +14,14 @@ public class Reverse {
             System.out.print(c);
         }
 
-        char temp;
-        int left, right;
         right = text.length - 1;
-        for (left = 0;left<right;left++,right--) {
+        for (left = 0;left<right;left++,right--) { //two iterations in 1 for-loop.
             temp = text[left];
             text[left] = text[right];
             text[right] = temp;
         }
-        System.out.println();
+
+        System.out.println(); //to put the two below each other.
 
         for (char c : text) {
             System.out.print(c);
