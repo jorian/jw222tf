11910c073f8219ac129b5179c2cf96718ea75ff9
Jorian <jorian@outlook.com>
2016-11-24 19:59:58 +0100

    Commit

--- src/jw222tf_assign2/CountDigits.java
+++ src/jw222tf_assign2/CountDigits.java
@@ -31,26 +31,3 @@ public class CountDigits {
         }
     }
 }
-
-//        String numbers = sc.nextLine();                             //what if it is negative
-//        int temp=0,zero=0, odd=0, even=0;
-//
-//        if (numbers.matches("[0-9]+")) {
-//            if ((Integer.valueOf(numbers) > 0)) {
-//                for(int i = 0;i < numbers.length();i++) {
-//                    temp = (numbers.charAt(i) - '0'); //why did i use a '0'? > get numericalvalue
-//                    if (temp == 0) {
-//                        zero++;
-//                    } else if(temp%2==0) {
-//                        even++;
-//                    } else {
-//                        odd++;
-//                    }
-//                }
-//                System.out.print("Zeros: \t"+zero+"\nOdds: \t"+odd+"\nEvens:\t"+even);
-//            } else
-//                System.out.println("This integer is negative. Please correct.");
-//        } else
-//            System.out.println("This integer contains a non-numeric character. Please correct.");
-//    }
-//}
