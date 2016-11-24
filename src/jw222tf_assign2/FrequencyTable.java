ba718fd38f26415a7ce8127240c32f44124c6b95
Jorian <jorian@outlook.com>
2016-11-24 20:13:07 +0100

    Commit

--- src/jw222tf_assign2/FrequencyTable.java
+++ src/jw222tf_assign2/FrequencyTable.java
@@ -1,6 +1,7 @@
 package jw222tf_assign2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -11,10 +12,11 @@ public class FrequencyTable {
     public static void main(String[] args) {
         Random rand = new Random();
         int diceValue;
-        ArrayList intList = new ArrayList<>(6);
+        ArrayList intList = new ArrayList<>(Collections.nCopies(6,0)); //6);
 
-        for (int i=0; i<6; i++)
-            intList.add(i, 0);
+        // Almost everywhere I look on the internet, I find type List instead of ArrayList.
+
+        while(intList.size() < 6) intList.add(0);
 
         for (int i = 0; i<6000; i++) {
             diceValue = rand.nextInt(6);
@@ -22,11 +24,7 @@ public class FrequencyTable {
         }
 
         for (int i=1; i<7; i++) {
-            //Print 1. first array thingie.
+            System.out.println(i + ": " + intList.get(i-1)); //Print 1. first array thingie.
         }
-
-
-        System.out.println(intList);
-
     }
 }
