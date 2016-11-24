c0e64f690b56d7c7bcb055fb09d078b7f0e251b8
Jorian <jorian@outlook.com>
2016-11-24 20:19:02 +0100

    Commit

--- src/jw222tf_assign2/FrequencyTable.java
+++ src/jw222tf_assign2/FrequencyTable.java
@@ -12,15 +12,16 @@ public class FrequencyTable {
     public static void main(String[] args) {
         Random rand = new Random();
         int diceValue;
-        ArrayList intList = new ArrayList<>(Collections.nCopies(6,0)); //6);
+        // Because there is a difference between the size and the capacity of an ArrayList, I need to add elements (of 0)
+        // to the ArrayList. I can do this by using a for loop and set every element to 0, but I used a different
+        // approach here to keep it nice.
+        ArrayList intList = new ArrayList<>(Collections.nCopies(6,0));
 
         // Almost everywhere I look on the internet, I find type List instead of ArrayList.
 
-        while(intList.size() < 6) intList.add(0);
-
         for (int i = 0; i<6000; i++) {
-            diceValue = rand.nextInt(6);
-            intList.set(diceValue, (int) intList.get(diceValue)+1);
+            diceValue = rand.nextInt(6); //Get a number between 0 and 5
+            intList.set(diceValue, (int) intList.get(diceValue) + 1); //add the value to the correct ArrayList element.
         }
 
         for (int i=1; i<7; i++) {
