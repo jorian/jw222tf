1976bbf7122bc2234adb1e081d4a680f13d1c587
Jorian <jorian@outlook.com>
2016-11-27 12:22:56 +0100

    Commit

--- src/jw222tf_assign2/CountDigits.java
+++ src/jw222tf_assign2/CountDigits.java
@@ -10,13 +10,24 @@ public class CountDigits {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a large, positive integer: ");
 
-        int temp, zero=0, even=0, odd=0, arbitraryNumber = sc.nextInt();
+        // To account for LONG integers, I use the long instead of int. Still, very large numbers do not work properly.
+        // For that, BigNumber package should be used.
+        long arbitraryNumber = sc.nextLong();
+        int temp, zero=0, even=0, odd=0;
+
+        //I first convert the integer value to a string, in order to iterate through the numbers separately:
         String numbers = String.valueOf(arbitraryNumber);
         sc.close();
 
+        // Only start the iteration if the integer is positive:
         if (arbitraryNumber > 0) {
             for (int i=0;i<numbers.length();i++) {
-                temp = numbers.charAt(i);
+                /*
+                I have to account for the '0' in order to have the correct number. The char '0' casted to an integer
+                actually is 48. So a '1' will be 49, a '2' will be 50.
+                I basically cast the character to an integer this way. Found on stackoverflow.
+                */
+                temp = numbers.charAt(i) - '0';
                 if (temp == 0) {
                     zero++;
                 } else if (temp%2==0) {
