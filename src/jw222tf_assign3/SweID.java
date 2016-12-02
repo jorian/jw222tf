c88ac79f91d9b8434f4b104f1fea90831fe3bbc0
jorian <jorian@outlook.com>
2016-12-02 23:57:29 +0100

    Initial commit

--- src/jw222tf_assign3/SweID.java
+++ src/jw222tf_assign3/SweID.java
@@ -3,46 +3,55 @@ package jw222tf_assign3;
 /**
  * Created by JorianWielink on 01/12/2016.
  */
+
 public class SweID {
     public static void main(String[] args) {
-        String idNumber1 = "911029-3249";
+        String idNumber1 = "911029-2342"; //3249
         String idNumber2 = "640123-8826";
-        String idNumber3 = "551414-0913";
+        String idNumber3 = "550414-0913";
 
-        System.out.println(getFirstPart(idNumber1));
-        System.out.println(getLastPart(idNumber2));
+        System.out.println("The first part of the idnumber: " + getFirstPart(idNumber1));
+        System.out.println("The second part of the idnumber: " + getLastPart(idNumber2));
 
-        System.out.println("\n" + isFemaleNumber(idNumber2));
-        System.out.println(isFemaleNumber(idNumber3));
+        System.out.println("\nFemale number: " + isFemaleNumber(idNumber2));
+        System.out.println("Female number: " + isFemaleNumber(idNumber3));
 
-        System.out.println("\n" + isMaleNumber(idNumber2));
-        System.out.println(isMaleNumber(idNumber3));
+        System.out.println("\nMale number: " + isMaleNumber(idNumber2));
+        System.out.println("Male number: " + isMaleNumber(idNumber3));
 
-        System.out.println("\n" + areEqual(idNumber1, idNumber2));
-        System.out.println(areEqual(idNumber1, idNumber1));
+        System.out.println("\nAre id numbers equal: " + areEqual(idNumber1, idNumber2));
+        System.out.println("Are id numbers equal: " + areEqual(idNumber1, idNumber1));
 
-        System.out.println("\n" + isCorrect(idNumber3));
+        System.out.println(idNumber1 + " is a correct number: " + isCorrect(idNumber1));
+        System.out.println(idNumber2 + " is a correct number: " + isCorrect(idNumber2));
+        System.out.println(idNumber3 + " is a correct number: " + isCorrect(idNumber3));
     }
+
 
     private static String getFirstPart(String idNumber){
         return idNumber.substring(0,6);
     }
 
+
     private static String getLastPart(String idNumber){
         return idNumber.substring(7,11);
     }
+
 
     private static boolean isFemaleNumber(String idNumber) {
         return (Integer.valueOf(getLastPart(idNumber).substring(0,3)) % 2 == 0);
     }
 
+
     private static boolean isMaleNumber(String idNumber) {
         return (Integer.valueOf(getLastPart(idNumber).substring(0,3)) % 2 != 0); //learned about the difference between 'int' and 'Integer' here.
     }
 
+
     private static boolean areEqual(String idNumber1, String idNumber2) {
         return idNumber1.equals(idNumber2);
     }
+
 
     //date correct (y, m, d)
     //last part is correct according to rules on wikipedia.
@@ -65,7 +74,7 @@ public class SweID {
                 max = 31;
                 break;
             case 2:
-                if (Integer.valueOf(idNumber.substring(0, 2)) % 4 == 0) {
+                if (isLeapYear(getFirstPart(idNumber))) {
                     max = 29;
                     break;
                 }
@@ -118,4 +127,14 @@ public class SweID {
     }
 
 
+    private static boolean isLeapYear(String date) { //in format of YYMMDD.
+        int year = Integer.valueOf(date.substring(0,2));
+        if (year <= 16) {
+            year += 2000;
+        }
+        else {
+            year += 1900;
+        }
+        return ((year % 4 == 0) && (year % 100 != 0)) | (year % 400 == 0);
+    }
 }
