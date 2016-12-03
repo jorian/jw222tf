b10fc44c1e6e0356c936ea06e2d101973efe0205
jorian <jorian@outlook.com>
2016-12-03 22:06:31 +0100

    Initial commit

--- src/jw222tf_assign3/AlarmMain.java
+++ src/jw222tf_assign3/AlarmMain.java
@@ -15,3 +15,6 @@ public class AlarmMain {
         alarm.displayTime();
     }
 }
+
+//Only change I made in the AlarmClock.java was the line break type, since I am using a mac. Changed it to LF, was CRLF.
+//http://stackoverflow.com/questions/1552749/difference-between-cr-lf-lf-and-cr-line-break-types
