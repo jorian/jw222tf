3f5fd83e8841c943b7cf4944137d0bf9e106bdc0
Jorian <jorian@outlook.com>
2016-12-07 15:35:26 +0100

    Commit

--- src/jw222tf_assign3/Fraction.java
+++ src/jw222tf_assign3/Fraction.java
@@ -7,17 +7,27 @@ public class Fraction {
     private int numerator;
     private int denominator;
 
+    /*
+    Constructors:
+     */
     Fraction(){
         numerator = 1;
         denominator = 1;
     }
 
     Fraction(int num, int denom){
+        /*
+        Since this code is the same as the method setFraction, can I just call setFraction here?
+         */
         numerator = num;
         if (denomNotZero(denom)) {
             denominator = denom;
         }
     }
+
+    /*
+    The two get-methods:
+     */
 
     public int getNumerator(){
         return numerator;
@@ -27,17 +37,37 @@ public class Fraction {
         return denominator;
     }
 
-    private void setFraction(int num, int denom) {
+    /*
+    Created a setFraction, sets numerator and denominator to inputted integers:
+     */
+    public void setFraction(int num, int denom) {
         numerator = num;
         if (denomNotZero(denom)) {
             denominator = denom;
         }
     }
 
-    private boolean denomNotZero(int denom){
-        return (denom > 0) || (denom < 0);
+    /*
+    A setFraction, sets the fraction according to an other Fraction object:
+     */
+    public void setFraction(Fraction fr1) {
+        numerator = fr1.numerator;
+        denominator = fr1.denominator;
     }
 
+    /*
+    Checks if the denominator is more or less than 0.
+     */
+    private boolean denomNotZero(int denom){
+        if ((denom > 0) || (denom < 0)) {
+            return true;
+        }
+        System.err.println("The denominator must be more or less than 0.");
+        return false;
+    }
+    /*
+    Converts object variables to a readable String format
+     */
     public String toString() {
         return numerator + "/" + denominator;
     }
@@ -47,7 +77,15 @@ public class Fraction {
         Assuming both the numerator and the denominator can be negative.
         They cannot both be negative though, then they would actually be positive (multiply fraction with -1)
          */
-        return (numerator < 0) || (denominator < 0);
+        return (numerator < 0) ^ (denominator < 0);
+    }
+
+    public void makeEven(Fraction fr1, Fraction fr2) {
+        int commonDivisor = commonDivisor(fr1.denominator, fr2.denominator);
+        fr1.numerator = (fr1.numerator * (commonDivisor / fr1.denominator));
+        fr2.numerator = (fr2.numerator * (commonDivisor / fr2.denominator));
+        fr1.denominator = commonDivisor;
+        fr2.denominator = commonDivisor;
     }
 
     public void add(int wholeNumber) {
@@ -62,18 +100,22 @@ public class Fraction {
     }
 
     public void add(int num, int denom) {
-        if (denomNotZero(denom)) {
-            if (denom != denominator) {
-                int gcd = commonDivisor(denom, denominator);
-                int newNum1 = num * (gcd / denom);
-                int newNum2 = numerator * (gcd / denominator);
-                setFraction(newNum1 + newNum2, gcd);
-            } else {
-                numerator += num;
-            }
-        } else {
-            System.err.println("The denominator must be more or less than 0.");
+        Fraction fr1 = new Fraction(num, denom);
+        Fraction fr2 = new Fraction(numerator, denominator);
+        if (fr1.denominator != fr2.denominator) {
+            makeEven(fr1, fr2);
         }
+        numerator = fr1.numerator + fr2.numerator;
+        denominator = fr1.denominator;
+    }
+
+    public void add(Fraction fr1) {
+        Fraction fr2 = new Fraction(numerator, denominator);
+        if (fr1.denominator != fr2.denominator) {
+            makeEven(fr1, fr2);
+        }
+        numerator = fr1.numerator + fr2.numerator;
+        denominator = fr1.denominator;
     }
 
     public void subtract() {
--- src/jw222tf_assign3/FractionMain.java
+++ src/jw222tf_assign3/FractionMain.java
@@ -9,17 +9,24 @@ public class FractionMain {
         Fraction br = new Fraction(1,4);
         Fraction cr = new Fraction(5, -8);
         Fraction dr = new Fraction(-5, 8);
+        Fraction er = new Fraction(-9,-12);
 
+        System.out.println("String ar: " + ar.toString());
+        System.out.println("String br: " + br.toString());
+        System.out.println("String cr: " + cr.toString() + ": Negative? " + cr.isNegative());
+        System.out.println("String dr: " + dr.toString() + ": Negative? " + dr.isNegative());
+        System.out.println("String er: " + er.toString() + ": Negative? " + er.isNegative());
+
+
+        ar.setFraction(br);
         System.out.println(ar.toString());
-        System.out.println(br.toString());
-        System.out.println(cr.toString() + ": Negative? " + cr.isNegative());
-        System.out.println(dr.toString() + ": Negative? " + dr.isNegative());
 
-        br.add(3);
+        br.add(dr);
         System.out.println(br.toString());
 
-        ar.add(1,2);
-        System.out.println(ar.toString());
+        br.add(1, 4);
+        System.out.println(br.toString());
+
 
 
 
