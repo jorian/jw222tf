7883903459bc08060f57891aea19168b7381fe65
Jorian <jorian@outlook.com>
2017-01-06 14:45:18 +0100

    Commit

--- src/jw222tf_assign4/drunkard/DrunkenWalk.java
+++ src/jw222tf_assign4/drunkard/DrunkenWalk.java
@@ -1,5 +1,6 @@
 package jw222tf_assign4.drunkard;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -7,23 +8,61 @@ import java.util.Scanner;
  */
 public class DrunkenWalk {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size: ");
-        int sizeOfPlatform = sc.nextInt();
-        System.out.print("\nEnter the number of steps: ");
-        int maxSteps = sc.nextInt();
-        System.out.print("\nEnter the number of walks: ");
-        int partyPeople = sc.nextInt();
         int fellIntoWater = 0;
+        int partyPeople = 0;
 
-        for (int i = 0; i < partyPeople; i++) {
-            RandomWalk drunk = new RandomWalk(maxSteps, sizeOfPlatform);
-            drunk.walk();
-            if (!drunk.inBounds()) {
-                fellIntoWater++;
+        /*
+        Found this: http://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+        to automatically close the scanner at the end of the statement.
+         */
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the size: ");
+            int sizeOfPlatform = 0;
+            try {
+                sizeOfPlatform = sc.nextInt();
+            } catch (InputMismatchException e) {
+                e.getMessage();
             }
+            if (sizeOfPlatform <= 0) {
+                throw new Exception("Size of platform should be a positive integer.");
+            }
+
+            System.out.print("Enter the number of steps: ");
+            int maxSteps = 0;
+            try {
+                maxSteps = sc.nextInt();
+            } catch (InputMismatchException e) {
+                e.getMessage();
+            }
+            if (maxSteps <= 0) {
+                throw new Exception("The maximum number of steps should be a positive integer.");
+            }
+
+            System.out.print("Enter the number of walks: ");
+            partyPeople = 0;
+            try {
+                partyPeople = sc.nextInt();
+            } catch (InputMismatchException e) {
+                e.getMessage();
+            }
+            if (partyPeople <= 0) {
+                throw new Exception("The number of walks should be a positive integer.");
+            }
+
+            for (int i = 0; i < partyPeople; i++) {
+                RandomWalk drunk = new RandomWalk(maxSteps, sizeOfPlatform);
+                drunk.walk();
+                if (!drunk.inBounds()) {
+                    fellIntoWater++;
+                }
+                System.out.println(drunk.toString());
+            }
+
+            System.out.printf("Out of %d people, %d (%.2f%%) fell into the water.",
+                    partyPeople, fellIntoWater, ((double) fellIntoWater / partyPeople * 100));
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.printf("Out of %d people, %d (%.2f%%) fell into the water.",
-                partyPeople, fellIntoWater, ((double) fellIntoWater / partyPeople * 100));
     }
 }
